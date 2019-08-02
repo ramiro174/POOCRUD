d76b502fb11eefac31e5275672ea7e9118d54d8a
Ramiro Esquivel Duran <ramiro174@gmail.com>
2019-08-02 01:24:03 -0500

    verificando php

--- public/miphp.php
+++ public/miphp.php
@@ -1,13 +1,8 @@
 <?php
-/*$nombre=$_GET["nombre"];
-$edad=$_GET["edad"];
-*/
-    
-    error_reporting(E_ALL);
-    ini_set('display_errors', '1');
-phpinfo();
-echo json_encode(["text"=>"hola"]);
 
-/*echo "Hola ".$nombre. " tu edad es ->".$edad ;*/
+
+phpinfo();
+
+
 
 ?>
