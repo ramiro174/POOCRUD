26458d584d0a2a12a403bc8b3cd9ad59533571fd
Ramiro Esquivel Duran <ramiro174@gmail.com>
2019-08-05 20:06:48 -0500

    display error

--- public/crearPersona.php
+++ public/crearPersona.php
@@ -1,30 +1,30 @@
 <?php
     
     namespace proyecto;
+
     
-    use Exception;
-    use function print_r;
-    error_reporting(E_ALL);
-    ini_set('error_reporting', E_ALL);
-    try{
-    require("../vendor/autoload.php");
-   
-        $p = new Persona();
-        $p->nombre = "Juanitos";
-        $p->edad = "22";
-        $p->apellido_paterno = "Esquivel";
-        $p->apellido_materno = "Duran";
-        $p->save();
-      print_r($p);
-   
-      
-    } catch (Exception $e) {
-        return $e->getMessage();
-    }
-    
-    
-    
-    
-    
-   
-    
+    echo "ramiro";
+//    use Exception;
+//
+//    try{
+//    require("../vendor/autoload.php");
+//
+//        $p = new Persona();
+//        $p->nombre = "Juanitos";
+//        $p->edad = "22";
+//        $p->apellido_paterno = "Esquivel";
+//        $p->apellido_materno = "Duran";
+//        $p->save();
+//      print_r($p);
+//
+//
+//    } catch (Exception $e) {
+//        return $e->getMessage();
+//    }
+//
+
+
+
+
+
+
