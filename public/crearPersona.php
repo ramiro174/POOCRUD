05d766666733c848313148a10d1c82147a831f40
Ramiro Esquivel Duran <ramiro174@gmail.com>
2019-08-05 19:23:49 -0500

    verificando php

--- public/crearPersona.php
+++ public/crearPersona.php
@@ -1,28 +1,7 @@
 <?php
-    
-    namespace proyecto;
-    
-    use Exception;
-    
-    error_reporting(E_ALL);
-    error_reporting(-1);
-    ini_set('error_reporting', E_ALL);
-    try {
-    require("../vendor/autoload.php");
    
-        $p = new Persona();
-        $p->nombre = "Juanitos";
-        $p->edad = "22";
-        $p->apellido_paterno = "Esquivel";
-        $p->apellido_materno = "Duran";
-        $p->save();
-        print_r($p);
-    } catch (Exception $e) {
-        return $e->getMessage();
-    }
-    
-    
-    
-    
+   phpinfo();
    
-    
+   
+   
+   
